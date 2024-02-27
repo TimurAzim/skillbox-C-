827a3165484c4178681f8db5fefef316e786304b
Timur Azim <papaas73@gmail.com>
2024-02-27 20:41:56 +0600

    Create

---
+++ Skillbox 13.4/Helpers.h
@@ -0,0 +1,6 @@
+
+int square_of_sum(int num1, int num2) {
+    int sum_numbers = num1 + num2;
+    int square = sum_numbers * sum_numbers;
+    return square;
+}
---
+++ Skillbox 13.4/Skillbox 13.4.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include "Helpers.h"
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int result = square_of_sum(5, 8);
+    std::cout << "Квадрат суммы чисел равен: " << result << std::endl;
+    return 0;
+}
